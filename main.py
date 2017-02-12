1682da71e3e2f78c08b952aa019eb7d19b4fbc6d
Kevin Jungk <kjungk2@gmail.com>
2017-02-12 10:03:44 -0600

    new posts working; 5 recent blogs displaying; basic styling done

--- main.py
+++ main.py
@@ -1,24 +1,74 @@
 import webapp2
 import jinja2
 import os
+from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                         autoescape = True)
 
-class MainHandler(webapp2.RequestHandler):
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.write(*a, **kw)
+
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+
+
+class BlogPost(db.Model):
+    title = db.StringProperty(required=True)
+    post = db.TextProperty(required=True)
+    post_preview = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
+
+class MainHandler(Handler):
+    def render_main(self, blogposts):
+        self.render("mainblog.html", blogposts=blogposts)
+
     def get(self):
-        t = jinja_env.get_template("base.html")
+        blogposts = db.GqlQuery("SELECT * FROM BlogPost ORDER BY created DESC LIMIT 5")
+        self.render_main(blogposts)
+
+
+class NewPostHandler(Handler):
+    def render_newpost(self, title="", body="", error=""):
+        self.render("newpost.html", title=title, body=body, error=error)
+
+    def get(self):
+        t = jinja_env.get_template("newpost.html")
         content = t.render()
         self.response.write(content)
 
     def post(self):
-        new_post = self.request.get("new_post")
-        if new_post:
-            self.response.write("thanks for the new post")
-        else:
-            self.response.write("you didn't type anything!")
+        new_title = self.request.get("title")
+        new_body = self.request.get("body")
+
+        if not new_title:
+            error = "Provide a Title"
+            self.render_newpost(new_title, new_body, error)
+
+        elif not new_body:
+            error = "Provide some content"
+            self.render_newpost(new_title, new_body, error)
+
+        elif new_title and new_body:
+            b = BlogPost(title = new_title, post = new_body, post_preview = new_body[:30] + "...")
+            b.put()
+            self.redirect("/blog")
+
+
+class AutoRedirect(webapp2.RequestHandler):
+    def get(self):
+        self.redirect("/blog")
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', AutoRedirect),
+    ('/blog', MainHandler),
+    ('/newpost', NewPostHandler)
 ], debug=True)
